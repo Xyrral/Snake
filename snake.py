dbe3cbb7b466f5670877aab6f97d5c329c654863
Xyrral <dragonstarx45@gmail.com>
2017-12-06 00:58:12 +0100

    mode 1 joueur ok

--- snake.py
+++ snake.py
@@ -7,7 +7,18 @@ taille_case = 15
 largeur_plateau = 40  # en nombre de cases
 hauteur_plateau = 30  # en nombre de cases
 
-
+def deplacement(pos, direction, signe):
+    """
+    Fonction qui recoit la position accompagné d'une diréction et renvoie
+    la position actualisée
+    """
+    x, y = pos
+    s, d = direction
+    if signe == 1:
+        nPos = (x + s, y + d)
+    else:
+        nPos = (x - s, y - d)
+    return nPos
 def case_vers_pixel(x):
     """
     Fonction recevant les coordonnées d'une case du plateau sous la forme
@@ -27,11 +38,14 @@ def affiche_pommes(pommes):
 
 
 def affiche_serpent(serpent):
-    x, y = case_vers_pixel(serpent)  # à modifier !!!
-    
-    cercle(x, y, taille_case/2 + 1,
-           couleur='White', remplissage='White')
-def change_direction(direction, touche):
+    n = 0
+    while n < len(serpent):
+        x, y = case_vers_pixel(serpent[n])
+
+        cercle(x, y, taille_case/2 + 1,
+               couleur='Black', remplissage='Black')
+        n += 1
+def change_direction(direction, touche,):
     """Permet de gérer le changement de direction"""
     if touche == 'Up':
         # flèche haut pressée
@@ -41,7 +55,7 @@ def change_direction(direction, touche):
         return(0, 1)
     elif touche == 'Left':
         # flèche gauche pressée
-        return(-1, 0)
+        return(-1 , 0)
     elif touche == 'Right':
         # flèche droite pressée
         return(1, 0)
@@ -49,11 +63,9 @@ def change_direction(direction, touche):
         # pas de changement !
         return direction
 
-
-# programme principal
-if __name__ == "__main__":
-
+def ft_solo_mode():
     # initialisation du jeu
+    i = 0
     framerate = 10    # taux de rafraîchissement du jeu en images/s
     direction = (0, 0)  # direction initiale du serpent
     posS = (0,0)
@@ -62,22 +74,35 @@ if __name__ == "__main__":
                  taille_case * hauteur_plateau)
     # boucle principale
     pas_pomme = True
-    pos_S = [(0,0)]
+    pos_S = [(21,15)]
     while True:
         # affichage des objets
         efface_tout()
-        if posS == posP:
-            nb_pomme.append(1)
+        if pos_S[0] == posP:
             pas_pomme = True
+            pos_S.append(deplacement(posP, direction, 0))
+        collision = pos_S.count(pos_S[0])
+        if collision != 1:
+            break
         if pas_pomme == True:
-            px = randint(0, 39)
-            py = randint(0, 29)
+            px = randint(1, 38)
+            py = randint(1, 28)
             pas_pomme = False
             posP = px, py
-        affiche_pommes((px,py))# à modifier !
-        x, y = posS 
-        s, d = direction
-        posS = (x + s, y + d)
+            while pos_S.count(posP) != 0:
+                px = randint(1, 39)
+                py = randint(1, 29)
+                posP = px, py
+        affiche_pommes(posP)# à modifier !
+        if len(pos_S) > 1:
+            pos_S.insert(0, (deplacement(pos_S[0], direction, 1)))
+            pos_S.pop()
+        else:
+            pos_S[0] = (deplacement(pos_S[0], direction, 1))
+        x, y = pos_S[0]
+        print(pos_S)
+        if x > largeur_plateau or x < 0 or y > hauteur_plateau or y < 0:
+            break
         affiche_serpent(pos_S)  # à modifier !
         mise_a_jour()
 
@@ -85,7 +110,7 @@ if __name__ == "__main__":
         # gestion des événements
         ev = donne_evenement()
         ty = type_evenement(ev)
-        if ty == 'Quitte':
+        if ty == 'Escape':
             break
         elif ty == 'Touche':
             print(touche(ev))
@@ -96,3 +121,10 @@ if __name__ == "__main__":
 
     # fermeture et sortie
     ferme_fenetre()
+
+
+# programme principal
+if __name__ == "__main__":
+    ft_solo_mode()
+
+
